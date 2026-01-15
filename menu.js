843ab8e01d1c849d8a0bbf833affd7be88e1c501
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-15 15:12:42 -0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,13 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+const dropdown = document.querySelector('.dropdown');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('open');
+});
+
+dropdownToggle.addEventListener('click', (e) => {
+  e.preventDefault();
+  dropdown.classList.toggle('open');
+});
