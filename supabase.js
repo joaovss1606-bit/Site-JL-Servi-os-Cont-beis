6fa227a8ca57b48eeac76866efd0dc2fd8c8339e
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-16 10:44:31 -0300

    Update supabase.js
    fix supabase env for github pages

--- supabase.js
+++ supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
+const SUPABASE_URL = 'https://SEU_ID.supabase.co'
+const SUPABASE_ANON_KEY = 'SUA_ANON_KEY_AQUI'
+
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  https://qdgsmnfhpbkbovptwwjp.supabase.co,
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3NtbmZocGJrYm92cHR3d2pwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODE1NDUzNiwiZXhwIjoyMDgzNzMwNTM2fQ.W5iFRYrGVhgc9ySXdBgl00A1K1lHyjAz5ZTIP7C4ZrA
 )
