5487bb05e598076cd7a08ebefbd5e4c27797b247
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-16 10:47:24 -0300

    Update supabase.js
    reset supabase file

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = 'https://SEU_ID.supabase.co'
-const SUPABASE_ANON_KEY = 'SUA_ANON_KEY_AQUI'
+const SUPABASE_URL = ''
+const SUPABASE_ANON_KEY = ''
 
 export const supabase = createClient(
   https://qdgsmnfhpbkbovptwwjp.supabase.co,
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3NtbmZocGJrYm92cHR3d2pwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODE1NDUzNiwiZXhwIjoyMDgzNzMwNTM2fQ.W5iFRYrGVhgc9ySXdBgl00A1K1lHyjAz5ZTIP7C4ZrA
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3NtbmZocGJrYm92cHR3d2pwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODE1NDUzNiwiZXhwIjoyMDgzNzMwNTM2fQ.W5iFRYrGVhgc9ySXdBgl00A1K1lHyjAz5ZTIP7C4ZrA)
 )
